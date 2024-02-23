a7c590917e95cf4b07718ffddf0be3f9cbecece7
AlejandroSastoque <fabio.sastoque@parking.net.co>
2024-02-23 11:20:38 -0500

    Area de los mensajes

--- lib/config/presentation/chat/chat_screen.dart
+++ lib/config/presentation/chat/chat_screen.dart
@@ -6,16 +6,39 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const Padding(
-          padding: EdgeInsets.all(3.0),
-          child: CircleAvatar(
-            backgroundImage: NetworkImage(
-                'https://cdn2.chicmagazine.com.mx/uploads/media/2021/07/13/megan-fox-foto-instagram-1.jpg'),
+        appBar: AppBar(
+          leading: const Padding(
+            padding: EdgeInsets.all(3.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://cdn2.chicmagazine.com.mx/uploads/media/2021/07/13/megan-fox-foto-instagram-1.jpg'),
+            ),
           ),
+          title: const Text('Mi amor ❤️'),
+          // centerTitle: true,
         ),
-        title: const Text('Mi amor ❤️'),
-        // centerTitle: true,
+        body: _ChatView());
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('Index: $index');
+              },
+            )),
+            Text('Mundo'),
+          ],
+        ),
       ),
     );
   }
